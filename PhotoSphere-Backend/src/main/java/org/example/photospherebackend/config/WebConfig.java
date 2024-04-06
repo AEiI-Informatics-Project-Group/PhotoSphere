8733cd6630ba2a7a427ee238e6e1cce2a570f147
Tomasz Żogała <tomasz.jakub.zogala@gmail.com>
2024-04-06 13:13:47 +0200

    Backend Fix

--- PhotoSphere-Backend/src/main/java/org/example/photospherebackend/config/WebConfig.java
+++ PhotoSphere-Backend/src/main/java/org/example/photospherebackend/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.example.projectflow.config;
+package org.example.photospherebackend.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
