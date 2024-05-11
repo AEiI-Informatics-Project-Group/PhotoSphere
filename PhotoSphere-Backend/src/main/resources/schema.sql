5cc88ad2a09b41901bc2f7bb416096b0cb64fb63
Tomasz Żogała <tomasz.jakub.zogala@gmail.com>
2024-05-11 23:47:46 +0200

    database email fix

--- PhotoSphere-Backend/src/main/resources/schema.sql
+++ PhotoSphere-Backend/src/main/resources/schema.sql
@@ -3,7 +3,7 @@ CREATE TABLE "users" (
                         "username" VARCHAR(255) NOT NULL,
                         "first_name" VARCHAR(255) NOT NULL,
                         "last_name" VARCHAR(255) NOT NULL,
-                        "email" VARCHAR(255) NOT NULL,
+                        "email" VARCHAR(255) NOT NULL UNIQUE,
                         "password" VARCHAR(255) NOT NULL,
                         "gender" VARCHAR(255) NOT NULL,
                         "day_of_birth" DATE NOT NULL
