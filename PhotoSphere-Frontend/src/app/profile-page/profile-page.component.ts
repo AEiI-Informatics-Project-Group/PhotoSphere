e641eb67c4e1f8c47e63a6f971cb662a6023996a
Sebastian Malisz <sebastianmalisz95@gmail.com>
2024-05-13 19:35:39 +0200

    profile-page-update

--- PhotoSphere-Frontend/src/app/profile-page/profile-page.component.ts
+++ PhotoSphere-Frontend/src/app/profile-page/profile-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {NavBarComponent} from "../nav-bar/nav-bar.component";
+import {AuthService} from "../services/auth.service";
 
 @Component({
   selector: 'app-profile-page',
@@ -12,4 +13,22 @@ import {NavBarComponent} from "../nav-bar/nav-bar.component";
 })
 export class ProfilePageComponent {
 
+  constructor(protected authService: AuthService){}
+
+  filterIconSrc: string = 'assets/icons/filter_category.png';
+  imageSrc: string = 'assets/icons/google-logo.png';
+  profileDescription: string = "dupadupadupa";
+  filterName: string = "Lake";
+
+  onFilterClick(): void {
+    console.log('Filter button clicked');
+  }
+
+  onImageArticleClick(): void {
+    console.log('Image article clicked');
+  }
+
+  onPlaceholderClick(): void {
+    console.log('Placeholder article clicked');
+  }
 }
