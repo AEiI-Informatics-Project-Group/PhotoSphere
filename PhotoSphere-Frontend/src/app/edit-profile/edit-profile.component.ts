a92c7a83acb4b098b11a12669075bab017413dea
unknown <mkoncowicz@gmail.com>
2024-05-14 13:07:44 +0200

    Profile page

--- PhotoSphere-Frontend/src/app/edit-profile/edit-profile.component.ts
+++ PhotoSphere-Frontend/src/app/edit-profile/edit-profile.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import {FormsModule, NgForm, ReactiveFormsModule} from "@angular/forms";
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-edit-profile',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './edit-profile.component.html',
   styleUrl: './edit-profile.component.css'
 })
