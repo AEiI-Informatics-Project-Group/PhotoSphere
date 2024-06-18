623d16993d25fcc2f158163e10acbfb1127c2005
unknown <mkoncowicz@gmail.com>
2024-06-18 12:27:52 +0200

    search bar update

--- PhotoSphere-Frontend/src/app/nav-bar/nav-bar.component.ts
+++ PhotoSphere-Frontend/src/app/nav-bar/nav-bar.component.ts
@@ -1,8 +1,10 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
 import {CommonModule} from "@angular/common";
 import {AuthService} from "../services/auth.service";
 import {FormsModule} from "@angular/forms";
+import {Tag} from "../models/tag.model";
+import {TagService} from "../services/tag.service";
 
 
 @Component({
@@ -12,14 +14,27 @@ import {FormsModule} from "@angular/forms";
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit {
   searchQuery: string = '';
+  allTags: Tag[] = [];
+  filteredTags: Tag[] = [];
 
   constructor(
     private router: Router,
-    public authService: AuthService
+    public authService: AuthService,
+    private tagService: TagService
   ) {}
 
+  ngOnInit() {
+    this.tagService.getAllTags().subscribe(
+      (tags: Tag[]) => {
+        this.allTags = tags;
+      },
+      (error) => {
+        console.error('Failed to load tags: ', error);
+      }
+    );
+  }
 
   onNavItemClick(item: string): void {
     console.log(`${item} clicked`);
@@ -46,5 +61,20 @@ export class NavBarComponent {
     if (event.key === 'Enter' && this.searchQuery.trim() !== '') {
       this.router.navigate(['/SearchResults'], { queryParams: { tag: this.searchQuery.trim() } });
     }
+  }
+
+  onSearchQueryChange(): void {
+    if (this.searchQuery.trim() === '') {
+      this.filteredTags = [];
+    } else {
+      const query = this.searchQuery.toLowerCase();
+      this.filteredTags = this.allTags.filter(tag => tag.name.toLowerCase().startsWith(query));
+    }
+  }
+
+  selectTag(tag: Tag): void {
+    this.searchQuery = tag.name;
+    this.filteredTags = [];
+    this.router.navigate(['/SearchResults'], { queryParams: { tag: this.searchQuery.trim() } });
   }
 }
